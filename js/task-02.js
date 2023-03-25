e8b2a791e927dae0d2752bbc924ac945662c5b14
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2023-03-25 13:25:13 +0200

    second_task-done.

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const itemListEl = ingredients.map((el) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = el;
+  itemEl.classList.add("item");
+
+  return itemEl;
+});
+
+listEl.append(...itemListEl);
